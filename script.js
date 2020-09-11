9d901239c0d3581449c6f19eb322a57ac9e0d068
Bridget Shannon <bridgetrshannon@gmail.com>
2020-09-11 16:56:52 -0400

    updated confirm statements for the four variables, need to first prompt for total characters though

--- script.js
+++ script.js
@@ -18,46 +18,50 @@ var length = parseInt(
 
   if (isNaN(length) === true) {
 alert("Password length must be a number");
-return
-
+return;
   }
 
   if (length < 8) {
     alert("Password length must be at least 8 characters!");
-    return
+    return;
   }
 
   if (length > 129) {
     alert("Password length must be no more than 129 characters!");
-    return
+    return;
   }
 
   if (length === "") {
     alert("Password must contan characters!");
-    return
+    return;
   }
+}
 
-// if (aka else) (user?)choice is between 8 and 129 characters
-//   then go to the next prompt
+var hasNumeric = confirm(
+  "Do you want your password to include numbers?")
+//   if (user?)choice = yes to numeric
+//   then include numeric array to choose from
+//   else (user?)choice = no to numeric
+//   then don't include numeric array to choose from
 
-  var lowercase = confirm("Do you want your password to include lowercase letters?")
+  var hasLowercase = confirm(
+    "Do you want your password to include lowercase letters?");
 //   if (user?)choice = yes to lowercase
 //   then include lowercase array to choose from
-
 //   else (user?)choice = no to lowercase
 //   then don't include lowercase array to choose from
 
-  var uppercase = confirm("Do you want your password to include uppercase letters?")
+  var hasUppercase = confirm(
+    "Do you want your password to include uppercase letters?");
 //   if (user?)choice = yes to uppercase
 //   then include uppercase array to choose from
-
 //   else (user?)choice = no to uppercase
 //   then don't include uppercase array to choose from
 
-  var symbols = confirm("Do you want your password to include special characters?")
+  var hasSymbols = confirm(
+    "Do you want your password to include special characters?");
 //   if (user?)choice = yes to symbols
 //   then include symbols array to choose from
-
 //   else (user?)choice = no to symbols
 //   then don't include symbols array to choose from
 
